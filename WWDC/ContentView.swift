ce089c3a1af8622f8ab77ceb98534af64efc1fe2
sharuban <s.s.ruban98@gmail.com>
2025-04-22 14:47:17 +0530

    changed

--- WWDC/ContentView.swift
+++ WWDC/ContentView.swift
@@ -9,32 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animate = false
-    
-    var body: some View {
-        ZStack {
-            // Background
-            LinearGradient(gradient: Gradient(colors: [.purple, .blue, .pink]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-            .ignoresSafeArea()
+        
+        var body: some View {
+            ZStack {
+                // Background
+                LinearGradient(gradient: Gradient(colors: [.purple, .blue, .pink]),
+                               startPoint: .topLeading,
+                               endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+                
+                // Animated WWDC Text
+                    Text("WWDC 25")
+                        .font(.system(size: 64, weight: .bold))
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [.white, .yellow, .orange],
+                                startPoint: animate ? .topLeading : .bottomTrailing,
+                                endPoint: animate ? .bottomTrailing : .topLeading
+                            )
+                        )
             
-            // Animated WWDC Text
-            Text("WWDC25")
-                .font(.system(size: 64, weight: .bold))
-                .foregroundStyle(
-                    LinearGradient(
-                        colors: [.white, .yellow, .orange],
-                        startPoint: animate ? .topLeading : .bottomTrailing,
-                        endPoint: animate ? .bottomTrailing : .topLeading
-                    )
-                )
-                .scaleEffect(animate ? 1.1 : 0.9)
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true),value: animate)
-                .onAppear{
-                    animate = true
-                }
+                    .scaleEffect(animate ? 1.1 : 0.9)
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true),value: animate)
+                    .onAppear{
+                        animate = true
+                
+                    }
+            }
         }
-    }
 }
 
 #Preview {
